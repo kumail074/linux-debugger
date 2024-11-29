498c030cbe5bf731151c4bd8d706fc28b1fcd7e7
kumail074 <kumail074@proton.me>
2024-11-30 03:50:32 +0500

    registers

--- src/dbgr.c
+++ src/dbgr.c
@@ -25,6 +25,55 @@
     int status; //last status of target process
 } debugger;*/
 
+enum reg {
+    rax, rbx, rcx, rdx,
+    rdi, rsi, rbp, rsp,
+    r8, r9, r10, r11,
+    r12, r13, r14, r15,
+    rip, rflags, cs,
+    orig_rax, fs_base,
+    gs_base,
+    fs, gs, ss, ds, es
+};
+
+const size_t n_registers = 27;
+
+typedef struct {
+    enum reg r;
+    int dwarf_r;
+    char *name;
+} reg_descriptor;
+
+const reg_descriptor g_register_description[n_registers] = {
+    { r15, 15, "r15" },
+    { r14, 14, "r14" },
+    { r13, 13, "r13" },
+    { r12, 12, "r12" },
+    { rbp,  6, "rbp" },
+    { rbx,  3, "rbx" },
+    { r11, 11, "r11" },
+    { r10, 10, "r10" },
+    {  r9,  9,  "r9" },
+    {  r8,  8,  "r8" },
+    { rax,  0, "rax" },
+    { rcx,  2, "rcx" },
+    { rdx,  1, "rdx" },
+    { rsi,  4, "rsi" },
+    { rdi,  5, "rdi" },
+    { orig_rax, -1, "orig_rax" },
+    { rip, -1, "rip" },
+    {  cs, 51,  "cs" },
+    { rflags, 49, "rflags" },
+    { rsp,  7, "rsp" },
+    {  ss, 52,  "ss" },
+    { fs_base, 58, "fs_base" },
+    { gs_base, 59, "gs_base" },
+    {  ds, 53,  "ds" },
+    {  es, 50,  "es" },
+    {  fs, 54,  "fs" },
+    {  gs, 55,  "gs" }
+};
+
 typedef struct {
     pid_t prog_pid;
     char *prog_name;
